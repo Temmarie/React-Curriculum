022cfbd73ae63cafcc25eafe8f9a55c31bc27ca5
Temmarie <temmar_whyte16@yahoo.com>
2024-10-02 23:31:30 +0100

    Project completion

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,9 @@ module.exports = {
    './counter-app/public/**/*.html',  // Include public HTML files
   './todo-app/public/**/*.html',
   './counter-app/src/**/*.{js,jsx,ts,tsx}',  // Source files
-  './todo-app/src/**/*.{js,jsx,ts,tsx}'
+  './todo-app/src/**/*.{js,jsx,ts,tsx}',
+  '/weather-app/src/**/*.{js,jsx}',
+  '/weather-app/public/**/*.html'
   ],
   theme: {
     extend: {},
---
+++ weather-app/src/main.jsx
@@ -0,0 +1,14 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import ErrorBoundary from './components/ErrorBoundary.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+     <ErrorBoundary>
+      <App />
+     </ErrorBoundary>
+
+  </StrictMode>,
+)
